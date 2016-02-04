1793ae6d265b0f56670494b81f6f626be1e35a39
am5800 <am5800@yandex.ru>
2016-02-04 09:29:54 +0100

    Compute L(v)

--- src/main/kotlin/sentenceBreaking/LogLinearSentenceBreakerTrainer.kt
+++ src/main/kotlin/sentenceBreaking/LogLinearSentenceBreakerTrainer.kt
@@ -1,5 +1,9 @@
-package ml.sentenceBreaking
+package sentenceBreaking
 
+import com.github.lbfgs4j.LbfgsMinimizer
+import com.github.lbfgs4j.liblbfgs.Function
+import ml.sentenceBreaking.LogLinearSentenceBreakerFeatureSet
+import ml.sentenceBreaking.SentenceBreakerTag
 import treebank.parsing.ParsePartOfSpeech
 import treebank.parsing.TreebankParserHandler
 import java.util.*
@@ -7,39 +11,95 @@ import java.util.*
 class TrainingTableEntry(val valueAtRegular: Boolean, val tag: SentenceBreakerTag)
 
 class LogLinearSentenceBreakerTrainer(private val featureSet: LogLinearSentenceBreakerFeatureSet) : TreebankParserHandler() {
-    private var currentWord: String? = null
+  private var currentWord: String? = null
 
-    private val queue = LinkedList<String>()
-    private val trainingData = ArrayList<List<TrainingTableEntry>>()
+  private val queue = LinkedList<String>()
+  private val trainingData = ArrayList<List<TrainingTableEntry>>()
 
-    override fun word(word: String, lemma: String, pos: ParsePartOfSpeech?) {
-        if (currentWord != null) {
-            queue.addLast(currentWord)
+  override fun word(word: String, lemma: String, pos: ParsePartOfSpeech?) {
+    if (currentWord != null) {
+      queue.addLast(currentWord)
+    }
+    process(SentenceBreakerTag.Regular)
+  }
+
+  override fun endSentence() {
+    if (currentWord != null) queue.push(currentWord)
+    currentWord = null
+    process(SentenceBreakerTag.SentenceBreak)
+  }
+
+  private fun process(tag: SentenceBreakerTag) {
+    if (queue.size < featureSet.requiredStackSize) return
+
+    while (queue.size > featureSet.requiredStackSize) queue.removeFirst()
+
+    val entries = featureSet.features
+            .map { it(queue, featureSet.currentWordOffset, tag) }
+            .map { TrainingTableEntry(getValueAtRegular(it, tag), tag) }
+
+    trainingData.add(entries)
+  }
+
+  private fun getValueAtRegular(it: Boolean, tag: SentenceBreakerTag) = if (tag == SentenceBreakerTag.Regular) it else !it
+
+  fun computeParameters(lambda: Double): DoubleArray {
+    val minimzer = LbfgsMinimizer()
+    return minimzer.minimize(CostFunction(lambda, trainingData))
+  }
+
+  class CostFunction(val lambda: Double, private val trainingData: ArrayList<List<TrainingTableEntry>>) : Function {
+
+    private val foundValues = trainingData.map {
+      it.map {
+        if (it.tag == SentenceBreakerTag.Regular) it.valueAtRegular else !it.valueAtRegular
+      }.toBooleanArray()
+    }.toTypedArray()
+
+    private val allValues: Array<Array<BooleanArray>> = trainingData.map {
+      it.map {
+        booleanArrayOf(it.valueAtRegular, !it.valueAtRegular)
+      }.toTypedArray()
+    }.toTypedArray()
+
+    private val n = trainingData.size
+    private val m = trainingData.first().size
+
+    override fun getDimension(): Int {
+      return m
+    }
+
+    override fun gradientAt(x: DoubleArray): DoubleArray {
+      throw UnsupportedOperationException()
+    }
+
+    override fun valueAt(x: DoubleArray): Double {
+      val linearTerm = foundValues.map { dot(x, it) }.sum()
+
+      var logTerm = 0.0
+      for (i in 0..n) {
+
+        var expTerm = 0.0
+        for (y in 0..allValues.first().first().size) {
+          val fValues = allValues[i].map { it[y] }.toBooleanArray()
+          val dotProduct = dot(x, fValues)
+          expTerm += Math.exp(dotProduct)
         }
-        process(SentenceBreakerTag.Regular)
+
+        logTerm += Math.log(expTerm)
+      }
+
+      val regularizationTerm = x.map { it * it }.sum() * lambda / 2
+
+      return linearTerm + logTerm - regularizationTerm
     }
 
-    override fun endSentence() {
-        if (currentWord != null) queue.push(currentWord)
-        currentWord = null
-        process(SentenceBreakerTag.SentenceBreak)
+    private fun dot(x: DoubleArray, y: BooleanArray): Double {
+      if (x.size != y.size) throw Exception("x and y size mismatch")
+
+      return x.mapIndexed { i, d -> if (y[i]) d else 0.0 }.sum()
     }
-
-    private fun process(tag: SentenceBreakerTag) {
-        if (queue.size < featureSet.requiredStackSize) return
-
-        while (queue.size > featureSet.requiredStackSize) queue.removeFirst()
-
-        val entries = featureSet.features
-                .map { it(queue, featureSet.currentWordOffset, tag) }
-                .map { TrainingTableEntry(getValueAtRegular(it, tag), tag) }
-
-        trainingData.add(entries)
-    }
-
-    private fun getValueAtRegular(it: Boolean, tag: SentenceBreakerTag) = if (tag == SentenceBreakerTag.Regular) it else !it
-
-    fun computeParameters(lambda: Double): List<Double> {
-        return emptyList()
-    }
+  }
 }
+
+
