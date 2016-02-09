cd1b5ae24a470ba9c457b18c29c7ae545b4d21ad
am5800 <am5800@yandex.ru>
2016-02-09 09:30:53 +0100

    Test LogLinear cost function

---
+++ src/main/kotlin/ml/sentenceBreaking/LogLinearCostFunction.kt
@@ -0,0 +1,73 @@
+package ml.sentenceBreaking
+
+import com.github.lbfgs4j.liblbfgs.Function
+
+class LogLinearCostFunction(val lambda: Double, private val trainingData: List<List<TrainingTableEntry>>) : Function {
+  private val n = trainingData.size
+  private val m = trainingData.first().size
+  private val Y = trainingData.first().first().values.size
+
+  override fun getDimension(): Int {
+    return m
+  }
+
+  override fun gradientAt(x: DoubleArray): DoubleArray {
+    val result = DoubleArray(m)
+
+    for (k in 0..m - 1) {
+      for (i in 0..n - 1) {
+        val valueAtK = trainingData[i][k].answer.toInt()
+        result[k] += valueAtK
+
+        var sum = 0.0
+        for (y in 0..Y - 1) {
+          val valueAtKGivenY = trainingData[i][k].by(y).toInt()
+          val p = computeP(y, i, x)
+
+          sum += valueAtKGivenY * p
+        }
+
+        result[k] -= Math.log(sum)
+      }
+
+      val regularizationTerm = x[k] * -lambda
+      result[k] -= regularizationTerm
+    }
+
+    return result
+  }
+
+  private fun computeP(y: Int, i: Int, vs: DoubleArray): Double {
+    val entries = trainingData[i]
+    val nom = Math.exp(dot(vs, entries.map { it.by(y) }.toBooleanArray()))
+
+    var denom = 0.0
+    for (yy in 0..Y - 1) {
+      denom += Math.exp(dot(vs, entries.map { it.by(yy) }.toBooleanArray()))
+    }
+
+    return nom / denom
+  }
+
+  override fun valueAt(x: DoubleArray): Double {
+    val linearTerm = trainingData.map { dot(x, it.map { it.answer }.toBooleanArray()) }.sum()
+
+    var logTerm = 0.0
+    for (i in 0..n - 1) {
+
+      var expTerm = 0.0
+      for (y in 0..Y - 1) {
+        val fValues = trainingData[i].map { it.by(y) }.toBooleanArray()
+        val dotProduct = dot(x, fValues)
+        expTerm += Math.exp(dotProduct)
+      }
+
+      logTerm -= Math.log(expTerm)
+    }
+
+    val regularizationTerm = x.map { it * it }.sum() * lambda / 2
+
+    return linearTerm + logTerm - regularizationTerm
+  }
+
+}
--- src/main/kotlin/ml/sentenceBreaking/LogLinearSentenceBreakerTrainer.kt
+++ src/main/kotlin/ml/sentenceBreaking/LogLinearSentenceBreakerTrainer.kt
@@ -1,7 +1,6 @@
 package ml.sentenceBreaking
 
 import com.github.lbfgs4j.LbfgsMinimizer
-import com.github.lbfgs4j.liblbfgs.Function
 import treebank.parsing.ParsePartOfSpeech
 import treebank.parsing.TreebankParserHandler
 import java.util.*
@@ -59,75 +58,10 @@ class LogLinearSentenceBreakerTrainer(private val featureSet: LogLinearSentenceB
 
   fun computeParameters(lambda: Double): DoubleArray {
     val minimizer = LbfgsMinimizer()
-    return minimizer.minimize(CostFunction(lambda, trainingData))
+    return minimizer.minimize(LogLinearCostFunction(lambda, trainingData))
   }
 
-  class CostFunction(val lambda: Double, private val trainingData: List<List<TrainingTableEntry>>) : Function {
-    private val n = trainingData.size
-    private val m = trainingData.first().size
-    private val Y = trainingData.first().first().values.size
-
-    override fun getDimension(): Int {
-      return m
-    }
-
-    override fun gradientAt(x: DoubleArray): DoubleArray {
-      val result = DoubleArray(m)
-
-      for (k in 0..m-1) {
-        for (i in 0..n-1) {
-          val valueAtK = trainingData[i][k].answer.toInt()
-          result[k] += valueAtK
-
-          for (y in 0..Y-1) {
-            val valueAtKGivenY = trainingData[i][k].by(y).toInt()
-            val p = computeP(y, i, x)
-
-            result[k] -= valueAtKGivenY * p
-          }
-        }
-
-        val regularizationTerm = x[k] * -lambda
-        result[k] -= regularizationTerm
-      }
-
-      return result
-    }
-
-    private fun computeP(y: Int, i: Int, vs: DoubleArray): Double {
-      val entries = trainingData[i]
-      val nom = Math.exp(dot(vs, entries.map { it.by(y) }.toBooleanArray()))
-
-      var denom = 0.0
-      for (yy in 0..Y-1) {
-        denom += Math.exp(dot(vs, entries.map { it.by(yy) }.toBooleanArray()))
-      }
-
-      return nom / denom
-    }
-
-    override fun valueAt(x: DoubleArray): Double {
-      val linearTerm = trainingData.map {dot(x, it.map {it.answer}.toBooleanArray() )}.sum()
-
-      var logTerm = 0.0
-      for (i in 0..n-1) {
-
-        var expTerm = 0.0
-        for (y in 0..Y-1) {
-          val fValues = trainingData[i].map {it.by(y)}.toBooleanArray()
-          val dotProduct = dot(x, fValues)
-          expTerm += Math.exp(dotProduct)
-        }
-
-        logTerm += Math.log(expTerm)
-      }
-
-      val regularizationTerm = x.map { it * it }.sum() * lambda / 2
-
-      return linearTerm + logTerm - regularizationTerm
-    }
-
-  }
 }
+
 
 
