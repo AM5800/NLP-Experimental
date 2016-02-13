72b562d31734d4dd99f410fe83cf7787f7bfd113
am5800 <am5800@yandex.ru>
2016-02-13 14:48:31 +0100

    Add logger

--- src/main/kotlin/Program.kt
+++ src/main/kotlin/Program.kt
@@ -5,6 +5,8 @@ import ml.sentenceBreaking.MEMM.LogLinearSentenceBreakerTrainer
 import ml.sentenceBreaking.SentenceBreakerPerformanceTester
 import ml.sentenceBreaking.SentenceBreakerTestDataMaker
 import ml.sentenceBreaking.SentenceBreakingHandler
+import org.joda.time.Duration
+import org.joda.time.format.PeriodFormatterBuilder
 import treebank.TreebankRepository
 import treebank.parsing.NegraParser
 import treebank.parsing.OnlyPlainSentenceSelector
@@ -13,14 +15,20 @@ import treebank.parsing.TreexParser
 import treebank.parsing.shuffling.RelativeRange
 import treebank.parsing.shuffling.TreebankShuffleRepository
 import java.io.File
+import java.util.logging.ConsoleHandler
+import java.util.logging.Formatter
+import java.util.logging.LogRecord
+import java.util.logging.Logger
 
 fun main(args: Array<String>) {
+  val logger = initLogger()
+
   val treebanksRepo = TreebankRepository(File("data\\corpuses\\"))
   val parsers = TreebankParsersSet()
   parsers.registerParser(NegraParser())
   parsers.registerParser(TreexParser())
 
-  val seedRepo = TreebankShuffleRepository(treebanksRepo, parsers)
+  val seedRepo = TreebankShuffleRepository(treebanksRepo, parsers, logger)
 
   seedRepo.shuffleIfNeeded(OnlyPlainSentenceSelector())
 
@@ -40,13 +48,57 @@ fun main(args: Array<String>) {
   parsers.parse(treebanksRepo.getTreebanks().first { it.treebankPath.absolutePath.contains("TIGER", true) },
           seedRepo.newHandler(handler, trainingRange),
           seedRepo.newHandler(maker, testRange),
-          seedRepo.newHandler(logLinearBreakerTrainer, RelativeRange(0, 1)))
+          seedRepo.newHandler(logLinearBreakerTrainer, trainingRange))
 
   val tester = SentenceBreakerPerformanceTester()
   val logLinearBreaker = LogLinearSentenceBreaker(logLinearBreakerTrainer.getTrainedFeatureSet(20.0))
   val heuristicBreakerPerformance = tester.getPerformance(heuristicBreaker, maker.getTestData())
-  val logLinearBreakerPerformance = tester.getPerformance(logLinearBreaker, maker.getTestData())
+  val logLinearBreakerPerformance = tester.getPerformance(logLinearBreaker, maker.getTestData(), true)
   println("Heuristic sentence breaking performance: $heuristicBreakerPerformance")
   println("LogLinear sentence breaking performance: $logLinearBreakerPerformance")
+}
+
+fun initLogger(): Logger {
+  val logger = Logger.getLogger("NLP")
+  logger.useParentHandlers = false
+  val console = ConsoleHandler()
+  console.formatter = CustomLoggerFormatter()
+  logger.addHandler(console)
+  return logger
+}
+
+class CustomLoggerFormatter : Formatter() {
+  private var lastTime: Long? = null
+
+  override fun format(record: LogRecord?): String? {
+    if (record == null) return null
+
+    val time = formatTime(record.millis)
+    return "$time: ${record.message}\n"
+  }
+
+  private fun formatTime(millis: Long): String {
+    val t = lastTime
+    val formatter = PeriodFormatterBuilder()
+            .printZeroAlways()
+            .minimumPrintedDigits(2)
+            .appendMinutes()
+            .appendSeparator(":")
+            .appendSeconds()
+            .appendSeparator(".")
+            .minimumPrintedDigits(3)
+            .appendMillis()
+            .toFormatter()
+
+    if (t == null) {
+      val duration = Duration(0)
+      lastTime = millis
+      return formatter.print(duration.toPeriod())
+    } else {
+      val duration = Duration(millis - t)
+      return "+" + formatter.print(duration.toPeriod())
+    }
+  }
+
 }
 
